35a10ae318c08eacccfbb72ef639d9acd6b88807
johnwick070 <87776597+johnwick070@users.noreply.github.com>
2021-07-21 22:55:09 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -119,7 +119,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '918921483992@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./sticker/song.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['Pikachu','Msd','Vijay','Rashmika','song','Sry','Da','Line']
+const array = ['Pikachu','Msd','chriz','Vijay','Rashmika','song','Sry','Da','Line']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
